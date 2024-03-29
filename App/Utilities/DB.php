016e03e9e883ec308eeafea40530f6cd8067b86e
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-03-30 01:12:50 +0800

    新增 DB Utility

---
+++ App/Utilities/DB.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Utilities;
+
+use App\Traits\Singleton;
+use PDO;
+use Exception;
+
+class DB
+{
+    use Singleton;
+
+    /**
+     * \PDO 類別實體物件
+     * 
+     * @var \PDO
+     */
+    private $pdo;
+
+    /**
+     * \PDOStatement 類別實體物件
+     * 
+     * @var \PDOStatement
+     */
+    private $stmt;
+
+    /**
+     * 建構式
+     * 
+     * @param \PDO $pdo
+     * 
+     * @return void
+     */
+    private function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * 關閉資料庫連線
+     * 
+     * @return void
+     */
+    public function disconnect()
+    {
+        $this->pdo = $this->stmt = null;
+    }
+
+    /**
+     * 執行 SQL statement
+     * 
+     * @param string $statement
+     * @param array|null $params
+     * 
+     * @return static|false
+     */
+    public function query(string $statement, ?array $params = null)
+    {
+        $this->stmt = $this->pdo->prepare($statement);
+        return $this->stmt->execute($params) ? $this : false;
+    }
+
+    /**
+     * 取得資料表受影響列數
+     * 
+     * @return int
+     */
+    public function affectedRowNums()
+    {
+        return isset($this->stmt) ? $this->stmt->rowCount() : 0;
+    }
+
+    /**
+     * 取得查詢資料
+     * 
+     * @return array|null
+     * 
+     * @throws \Exception
+     */
+    public function get()
+    {
+        if (!isset($this->stmt)) {
+            throw new Exception('Please execute SQL statement before get datas');
+        }
+
+        $datas = [];
+
+        while (($row = $this->stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
+            $datas[] = $row;
+        }
+
+        return $datas ?: null;
+    }
+}
