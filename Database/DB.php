2cdef2a611daff8b5e00fad8ffd151ef26276d77
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-03-30 01:11:44 +0800

    更新程式架構

--- Database/DB.php
+++ Database/DB.php
@@ -2,137 +2,26 @@
 
 namespace Database;
 
-use App\Traits\Singleton;
-use App\Enums\Database\Transaction;
-use PDO;
+use App\Containers\Container;
+use App\Utilities\DB as Database;
 use Exception;
-use ReflectionEnum;
 
 class DB
 {
-    use Singleton;
-
-    /**
-     * \PDO 類別實體物件
-     * 
-     * @var \PDO
-     */
-    private $pdo;
-
-    /**
-     * \PDOStatement 類別實體物件
-     * 
-     * @var \PDOStatement
-     */
-    private $stmt;
-
-    /**
-     * 建構式
-     * 
-     * @return void
-     */
-    private function __construct()
-    {
-    }
-
-    /**
-     * 連線資料庫
-     * 
-     * @return void
-     */
-    public function connect()
-    {
-        if (isset($this->pdo)) {
-            return;
-        }
-
-        $host = env('DB_HOST');
-        $port = env('DB_PORT');
-        $name = env('DB_NAME');
-        $charset = env('DB_CHARSET');
-        $user = env('DB_USER');
-        $password = env('DB_PASSWORD');
-        $dsn = "mysql:host=$host;port=$port;dbname=$name;charset=$charset";
-        $this->pdo = new PDO($dsn, $user, $password);
-    }
-
-    /**
-     * 關閉資料庫連線
-     * 
-     * @return void
-     */
-    public function disconnect()
-    {
-        $this->pdo = $this->stmt = null;
-    }
-
-    /**
-     * 執行 SQL statement
-     * 
-     * @param string $statement
-     * @param array|null $params
-     * 
-     * @return static|false
-     */
-    public function query(string $statement, ?array $params = null)
-    {
-        $this->stmt = $this->pdo->prepare($statement);
-        return $this->stmt->execute($params) ? $this : false;
-    }
-
-    /**
-     * 取得資料表受影響列數
-     * 
-     * @return int
-     */
-    public function affectedRowNums()
-    {
-        return isset($this->stmt) ? $this->stmt->rowCount() : 0;
-    }
-
-    /**
-     * 取得查詢資料
-     * 
-     * @return array|null
-     * 
-     * @throws \Exception
-     */
-    public function get()
-    {
-        if (!isset($this->stmt)) {
-            throw new Exception('Please execute SQL statement before get datas');
-        }
-
-        $datas = [];
-
-        while (($row = $this->stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
-            $datas[] = $row;
-        }
-
-        return $datas ?: null;
-    }
-
     /**
      * @param string $name
      * @param array $arguments
      * 
-     * @return bool
+     * @return mixed
      * 
      * @throws \Exception
      */
     public static function __callStatic(string $name, array $arguments)
     {
-        $reflector = new ReflectionEnum(Transaction::class);
-
-        if (!$reflector->hasCase(strtoupper($name))) {
-            throw new Exception('Call to undefined method ' . static::class . '::' . $name . '()');
+        if (!method_exists(Database::class, $name)) {
+            throw new Exception("Call to undefined method " . __CLASS__ . "::$name()");
         }
 
-        $statement = match (strtolower($name)) {
-            (Transaction::TRANSACTION)->value => 'start transaction',
-            (Transaction::ROLLBACK)->value => 'rollback',
-            (Transaction::COMMIT)->value => 'commit'
-        };
-        return static::getInstance()->query($statement) !== false;
+        return Container::get(Database::class)->$name(...$arguments);
     }
 }
