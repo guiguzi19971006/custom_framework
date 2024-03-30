069478e446367de98705dc91a9fa0512f5dd9d0f
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-03-31 00:07:07 +0800

    修改綁定服務的方式

--- public/index.php
+++ public/index.php
@@ -7,21 +7,25 @@ defined('APP_PATH') || define('APP_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR
 defined('PUBLIC_PATH') || define('PUBLIC_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 defined('STORAGE_PATH') || define('STORAGE_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR);
 
+$config = require_once '../config.php';
+
+use App\Bootstrap\Bootstrapping;
+
 require_once '../helper.php';
 require_once '../error_handler.php';
 require_once '../exception_handler.php';
 require_once '../autoload.php';
-require_once '../shutdown.php';
-require_once '../routes.php';
-$config = require_once '../config.php';
 
-// 註冊 Provider
-if (isset($config['providers']) && is_array($config['providers'])) {
-    foreach ($config['providers'] as $provider) {
-        $provider::bind();
-    }
+// 確認是否正確定義 Service Providers
+if (!isset($config['providers']) || !is_array($config['providers'])) {
+    throw new \Exception("Missing key 'providers' in " . dirname(__DIR__) . DIRECTORY_SEPARATOR . "config.php");
 }
 
-use App\Bootstrap\Bootstrapping;
+// 註冊服務
+Bootstrapping::services($config['providers']);
 
+require_once '../shutdown.php';
+require_once '../routes.php';
+
+// 處理請求
 Bootstrapping::init();
