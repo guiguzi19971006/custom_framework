e7df2bd497ef7a49c5e3992761b048f1ebbc20af
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-04-21 22:24:32 +0800

    更新 PUBLIC_PATH 常數

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Taipei');
 
 defined('ROOT_PATH') || define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 defined('APP_PATH') || define('APP_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'App' . DIRECTORY_SEPARATOR);
-defined('PUBLIC_PATH') || define('PUBLIC_PATH', __DIR__ . DIRECTORY_SEPARATOR);
+defined('PUBLIC_PATH') || define('PUBLIC_PATH', '/public');
 defined('STORAGE_PATH') || define('STORAGE_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR);
 defined('ENTRY_POINT_PATH') || define('ENTRY_POINT_PATH', '/public/index.php');
 
