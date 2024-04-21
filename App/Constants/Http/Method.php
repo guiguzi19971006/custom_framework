d2a7498d5ffb491a614a3a7b60ad752b579a4e52
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-04-21 10:14:58 +0800

    新增 Http Method 類別常數

---
+++ App/Constants/Http/Method.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants\Http;
+
+class Method
+{
+    const GET = 'GET';
+    const POST = 'POST';
+    const PUT = 'PUT';
+    const PATCH = 'PATCH';
+    const DELETE = 'DELETE';
+}
