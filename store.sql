287614406ac2b389881b8468a594800bb76b00bb
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-05-11 23:50:35 +0800

    更新 SQL

--- store.sql
+++ store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost
--- 產生時間： 2024 年 05 月 11 日 19:28
+-- 產生時間： 2024 年 05 月 11 日 23:49
 -- 伺服器版本： 10.6.3-MariaDB-log
 -- PHP 版本： 7.0.33
 
@@ -151,7 +151,6 @@ CREATE TABLE `user_interesting_product` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `user_id` bigint(20) UNSIGNED DEFAULT NULL COMMENT '使用者編號',
   `product_id` bigint(20) UNSIGNED NOT NULL COMMENT '產品編號',
-  `viewed_count` int(10) UNSIGNED NOT NULL COMMENT '產品被觀看次數',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '建立時間',
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '最後更新時間'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
@@ -160,14 +159,24 @@ CREATE TABLE `user_interesting_product` (
 -- 傾印資料表的資料 `user_interesting_product`
 --
 
-INSERT INTO `user_interesting_product` (`id`, `user_id`, `product_id`, `viewed_count`, `created_at`, `updated_at`) VALUES
-(1, NULL, 3, 3, '2024-05-09 16:00:23', '2024-05-11 10:18:17'),
-(2, NULL, 4, 3, '2024-05-09 16:00:58', '2024-05-09 17:41:01'),
-(3, NULL, 1, 4, '2024-05-09 16:01:13', '2024-05-11 11:26:53'),
-(4, NULL, 2, 3, '2024-05-09 17:40:37', '2024-05-11 10:36:14'),
-(5, NULL, 5, 3, '2024-05-11 10:51:15', '2024-05-11 11:05:03'),
-(6, NULL, 6, 2, '2024-05-11 11:05:57', '2024-05-11 11:06:04'),
-(7, NULL, 15, 1, '2024-05-11 11:25:55', '2024-05-11 11:25:55');
+INSERT INTO `user_interesting_product` (`id`, `user_id`, `product_id`, `created_at`, `updated_at`) VALUES
+(1, NULL, 1, '2024-05-11 15:39:34', '2024-05-11 15:39:34'),
+(2, NULL, 12, '2024-05-11 15:39:48', '2024-05-11 15:39:48'),
+(3, NULL, 12, '2024-05-11 15:40:05', '2024-05-11 15:40:05'),
+(4, NULL, 1, '2024-05-11 15:40:14', '2024-05-11 15:40:14'),
+(5, NULL, 9, '2024-05-11 15:42:41', '2024-05-11 15:42:41'),
+(6, NULL, 11, '2024-05-11 15:43:01', '2024-05-11 15:43:01'),
+(7, NULL, 11, '2024-05-11 15:43:22', '2024-05-11 15:43:22'),
+(8, NULL, 11, '2024-05-11 15:43:37', '2024-05-11 15:43:37'),
+(9, NULL, 1, '2024-05-11 15:43:44', '2024-05-11 15:43:44'),
+(10, NULL, 12, '2024-05-11 15:43:46', '2024-05-11 15:43:46'),
+(11, NULL, 12, '2024-05-11 15:44:19', '2024-05-11 15:44:19'),
+(12, NULL, 1, '2024-05-11 15:44:32', '2024-05-11 15:44:32'),
+(13, NULL, 2, '2024-05-11 15:44:34', '2024-05-11 15:44:34'),
+(14, NULL, 2, '2024-05-11 15:47:24', '2024-05-11 15:47:24'),
+(15, NULL, 4, '2024-05-11 15:48:05', '2024-05-11 15:48:05'),
+(16, NULL, 4, '2024-05-11 15:48:13', '2024-05-11 15:48:13'),
+(17, NULL, 7, '2024-05-11 15:49:12', '2024-05-11 15:49:12');
 
 --
 -- 已傾印資料表的索引
@@ -251,7 +260,7 @@ ALTER TABLE `user`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user_interesting_product`
 --
 ALTER TABLE `user_interesting_product`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- 已傾印資料表的限制式
