fdbddb66d2feeabaa253329266961a8d8e82023c
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-05-12 17:41:52 +0800

    新增資源

--- views/components/header.php
+++ views/components/header.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo PUBLIC_PATH; ?>/css/font-awesome.min.css">
+<link href="<?php echo PUBLIC_PATH; ?>/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo PUBLIC_PATH; ?>/css/style.css?t=<?php echo time(); ?>">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo PUBLIC_PATH; ?>/js/jquery.min.js"></script>
+<script src="<?php echo PUBLIC_PATH; ?>/js/bootstrap.bundle.min.js"></script>
