83c3f8cb30c85b0fba4d0a3cb646775f0ab6fcae
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-05-26 01:42:00 +0800

    更新 Validator

--- App/Validators/Validator.php
+++ App/Validators/Validator.php
@@ -7,16 +7,30 @@ use Exception;
 class Validator
 {
     /**
+     * @var array
+     */
+    private static array $filterTypes = [
+        'domain' => FILTER_VALIDATE_DOMAIN,
+        'email' => FILTER_VALIDATE_EMAIL,
+        'float' => FILTER_VALIDATE_FLOAT,
+        'int' => FILTER_VALIDATE_INT,
+        'ip' => FILTER_VALIDATE_IP,
+        'mac' => FILTER_VALIDATE_MAC,
+        'url' => FILTER_VALIDATE_URL
+    ];
+
+    /**
      * 資料格式驗證
      * 
      * @param array $input
      * @param array $patterns
+     * @param array $messages
      * 
      * @return array|null
      * 
      * @throws \Exception
      */
-    public static function errors(array $input, array $patterns)
+    public static function errors(array $input, array $patterns, array $messages = [])
     {
         $errors = [];
 
@@ -25,15 +39,21 @@ class Validator
                 continue;
             }
 
-            if (($isValid = preg_match($patterns[$key], $value)) === false) {
+            if (in_array($patterns[$key], array_keys(static::$filterTypes))) {
+                $isValid = filter_var($value, static::$filterTypes[$patterns[$key]]);
+            } else {
+                $isValid = preg_match($patterns[$key], $value);
+            }
+
+            if (preg_last_error() !== PREG_NO_ERROR) {
                 throw new Exception('Invalid regular expression');
             }
 
-            if ($isValid) {
+            if ($isValid === $value || ((bool) $isValid !== false)) {
                 continue;
             }
 
-            $errors[] = $key;
+            $errors[$key] = $messages[$key] ?? '';
         }
 
         return $errors ?: null;
