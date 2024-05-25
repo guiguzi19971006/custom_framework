e9a1c9699cd214bb2b8a6f2017e793100eea2c6f
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-05-26 01:49:29 +0800

    新增會員註冊成功的通知信件模板

---
+++ views/email/user/registration_successfully.php
@@ -0,0 +1,12 @@
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>會員註冊成功通知信</title>
+</head>
+<body>
+    <p>
+        親愛的<b><?php echo $name; ?>您好，您已完成會員註冊，祝您購物愉快!</b>
+    </p>
+</body>
+</html>
