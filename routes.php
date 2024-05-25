c5116dceb86182ba4a49b779e2c7063788b8b9f2
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-05-26 01:43:44 +0800

    新增會員註冊相關路由

--- routes.php
+++ routes.php
@@ -3,7 +3,11 @@
 use App\Supports\Route;
 use App\Controllers\HomeController;
 use App\Controllers\ProductController;
+use App\Controllers\UserController;
 
 Route::middleware(['csp'])->get('', [HomeController::class, 'index']);
 Route::middleware(['csp'])->get('/products', [ProductController::class, 'index']);
 Route::middleware(['csp'])->get('/products/{product_id}', [ProductController::class, 'show']);
+
+Route::middleware(['csp'])->get('/users/register', [UserController::class, 'register']);
+Route::post('/users/register', [UserController::class, 'registerProcess']);
