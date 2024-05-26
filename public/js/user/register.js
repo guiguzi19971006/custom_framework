2feda0fde054feff601383670ce37498e34c3d30
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-05-26 21:27:13 +0800

    更新會員註冊頁面

---
+++ public/js/user/register.js
@@ -0,0 +1,29 @@
+$(function () {
+    $('#user-registration-form').on('submit', function (event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: '/users/register',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'JSON',
+            success: function (data, status, xhr) {
+                $('#user-registration-form :input').removeClass('is-invalid');
+                $('div[id^="message-invalid-"]').text('');
+                alert(data.message);
+
+                switch (data.code) {
+                    case '000':
+                        location.href = '/';
+                        break;
+                    case '400':
+                        for (let key in data.errors) {
+                            $('#' + key).addClass('is-invalid')
+                            $('#message-invalid-' + key).text(data.errors[key]);
+                        }
+                        break;
+                }
+            }
+        });
+    });
+});
--- views/user/register.php
+++ views/user/register.php
@@ -14,16 +14,62 @@
         </div>
 
         <div class="bg-light p-3">
-            <form action="/users/register" method="post">
+            <form id="user-registration-form">
                 <div class="mb-3">
-                    <label for="email" class="form-label">電子郵件:</label>
-                    <input type="email" class="form-control" id="email" placeholder="請輸入電子郵件" name="email">
+                    <label for="name" class="form-label">真實姓名</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="請輸入真實姓名" aria-describedby="message-invalid-name">
+                    <div class="invalid-feedback" id="message-invalid-name"></div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="email" class="form-label">電子郵件</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="請輸入電子郵件" aria-describedby="message-invalid-email">
+                    <div class="invalid-feedback" id="message-invalid-email"></div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label">密碼</label>
+                    <input type="password" class="form-control" name="password" id="password" placeholder="請輸入密碼" aria-describedby="message-invalid-password">
+                    <div class="invalid-feedback" id="message-invalid-password"></div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="phone" class="form-label">手機號碼</label>
+                    <input type="text" class="form-control" name="phone" id="phone" placeholder="請輸入手機號碼" aria-describedby="message-invalid-phone">
+                    <div class="invalid-feedback" id="message-invalid-phone"></div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="birthday" class="form-label">生日</label>
+                    <input type="date" class="form-control" name="birthday" id="birthday" aria-describedby="message-invalid-birthday">
+                    <div class="invalid-feedback" id="message-invalid-birthday"></div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="gender" class="form-label">生理性別</label>
+                    <select name="gender" id="gender" class="form-select" aria-describedby="message-invalid-gender">
+                        <option selected>請選擇生理性別</option>
+                        <option value="M">男</option>
+                        <option value="F">女</option>
+                    </select>
+                    <div class="invalid-feedback" id="message-invalid-gender"></div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="address" class="form-label">住址</label>
+                    <textarea class="form-control" rows="5" cols="30" name="address" id="address" placeholder="請輸入住址"></textarea>
+                </div>
+
+                <div class="text-center">
+                    <button class="btn btn-dark" type="submit">註冊</button>
                 </div>
             </form>
         </div>
     </main>
 
     <?php view('components.footer'); ?>
+
+    <script src="<?php echo PUBLIC_PATH; ?>/js/user/register.js?t=<?php echo time(); ?>"></script>
 </body>
 
 </html>
