035df73b7401d4940bf5ecb05c9859e1d2bef256
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-06-02 19:14:33 +0800

    新增會員身分驗證頁面

---
+++ views/user/verify.php
@@ -0,0 +1,30 @@
+<html lang="zh-TW">
+
+<head>
+    <?php view('components.header'); ?>
+    <title>會員身分驗證</title>
+</head>
+
+<body>
+    <?php view('components.navbar'); ?>
+
+    <main>
+        <div class="text-bg-dark p-3 m-3 text-center">
+            <h1>會員身分驗證</h1>
+        </div>
+
+        <div class="text-bg-light p-3 m-3 text-center">
+            <p>
+                您已完成會員身分驗證
+            </p>
+        </div>
+
+        <div class="text-center">
+            <a href="/" class="btn btn-dark">回首頁</a>
+        </div>
+    </main>
+
+    <?php view('components.footer'); ?>
+</body>
+
+</html>
