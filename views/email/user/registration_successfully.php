993940643cbb6846d001d416f106d00220a7edbd
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-06-02 16:19:16 +0800

    更新會員註冊成功通知信內容

--- views/email/user/registration_successfully.php
+++ views/email/user/registration_successfully.php
@@ -8,7 +8,7 @@
 
 <body>
     <p>
-        親愛的 <b><?php echo $name; ?></b> 您好，您已完成會員註冊，請點擊下方連結以完成身分驗證:
+        親愛的 <b><?php echo $name; ?></b> 您好，您已完成會員註冊，請於 15 分鐘內點擊下方連結以完成會員身分驗證:
     </p>
     <a href="<?php echo env('SITE_URL'); ?>/users/verify/<?php echo $token; ?>">
         <?php echo env('SITE_URL'); ?>/users/verify/<?php echo $token; ?>
