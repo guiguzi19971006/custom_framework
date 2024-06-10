a18176a19b71b784bd51b10afd7fb201dd453b4f
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2024-06-11 00:45:15 +0800

    依據使用者登入狀態，調整 Navbar 的會員專區顯示內容

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,7 @@ $(function () {
         navbarUserAreaDropDownHtmlStr += '<li><a href="#" class="dropdown-item">優惠券</a></li>';
         navbarUserAreaDropDownHtmlStr += '<li><a href="#" class="dropdown-item">訂單查詢</a></li>';
     } else {
-        navbarUserAreaDropDownHtmlStr += '<li><a href="#" class="dropdown-item">會員註冊</a></li>';
+        navbarUserAreaDropDownHtmlStr += '<li><a href="/users/register" class="dropdown-item">會員註冊</a></li>';
     }
 
     $('nav ul.dropdown-menu').html(navbarUserAreaDropDownHtmlStr);
